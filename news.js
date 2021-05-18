75a1676f537a423ae73f71307b9d823456672cc9
kevinxshao <kevinxshao@gmail.com>
2021-05-17 22:44:07 -0400

    testing

--- news.js
+++ news.js
@@ -1,16 +1,3 @@
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    document.getElementById("user_div").style.display = "initial";
-    document.getElementById("login_div").style.display = "none";
-    window.alert("I'm in");
-  } else {
-    // No user is signed in.
-    document.getElementById("user_div").style.display = "none";
-    document.getElementById("login_div").style.display = "initial";
-    window.alert("no one is in");
-  }
-});
 function login(){
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
