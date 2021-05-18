83e8a574331339214237fb88cb1a425a47b9b6e1
kevinxshao <kevinxshao@gmail.com>
2021-05-17 22:27:48 -0400

    testing again

--- news.js
+++ news.js
@@ -1,17 +1,31 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-    document.getElementById("user_div").style.display = "block";
+    document.getElementById("user_div").style.display = "initial";
     document.getElementById("login_div").style.display = "none";
+    window.alert("I'm in");
   } else {
     // No user is signed in.
     document.getElementById("user_div").style.display = "none";
-    document.getElementById("login_div").style.display = "block";
+    document.getElementById("login_div").style.display = "initial";
+    window.alert("no one is in");
+  }
   }
 });
 function login(){
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
-
-    window.alert(userEmail +" " +userPass);
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      window.alert(user);
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert(errorMessage);
+    });   
+ 
 }
