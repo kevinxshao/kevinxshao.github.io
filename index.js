652b76d2391bc9f59c79ef6a2d040745eae29dee
kevinxshao <kevinxshao@gmail.com>
2021-05-17 21:27:45 -0400

    testing

--- index.js
+++ index.js
@@ -2,17 +2,11 @@ function login(){
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
 
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
-    .then((userCredential) => {
-      // Signed in
-      var user = userCredential.user;
-      // ...
-    })
-    .catch((error) => {
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
 
-      window.alert("Error : " + errorMessage);
+      window.alert("Error : " + errorMessage)
     });   
     window.alert(userEmail + " " + userPass);
 }
