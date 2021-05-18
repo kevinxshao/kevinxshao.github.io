c2871fef4d5437de3bcce22f762e696f48e0575c
kevinxshao <kevinxshao@gmail.com>
2021-05-17 21:20:13 -0400

    testing

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      document.getElementById("user_div").style.display = "block";
-      document.getElementById("login_div").style.display = "none";
-    } else {
-      // No user is signed in.
-      document.getElementById("user_div").style.display = "none";
-      document.getElementById("login_div").style.display = "block";
-    }
-  });
-
 function login(){
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
 
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      window.alert("Error : " + errorMessage);
+    });   
     window.alert(userEmail + " " + userPass);
 }
