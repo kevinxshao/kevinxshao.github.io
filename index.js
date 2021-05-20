9bc5fb3fc5b527bc0d6b80933ec1216ba165847c
kevinxshao <kevinxshao@gmail.com>
2021-05-19 21:20:53 -0400

    setting up login in main page

---
+++ index.js
@@ -0,0 +1,28 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      document.getElementById("user_div").style.display="initial";
+      document.getElementById("login_div").style.display="none";    
+    } else {
+      // No user is signed in.
+      document.getElementById("user_div").style.display="none";
+      document.getElementById("login_div").style.display="initial";    
+    }
+  });
+function login(){
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      window.alert("I'm in");
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert("I'm out");
+    });   
+ 
+}
