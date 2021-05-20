a016c746fa964b7f8fe6a336f84eda6500a8228f
kevinxshao <kevinxshao@gmail.com>
2021-05-19 22:08:41 -0400

    fixed a login error

--- news.js
+++ news.js
@@ -1,3 +1,14 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    document.getElementById("user_div").style.display="block";
+    document.getElementById("login_div").style.display="none";    
+  } else {
+    // No user is signed in.
+    document.getElementById("user_div").style.display="none";
+    document.getElementById("login_div").style.display="block";    
+  }
+});
 function login(){
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
@@ -14,4 +25,12 @@ function login(){
       window.alert("I'm out");
     });   
  
+}
+function logout(){
+  firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+    
 }
