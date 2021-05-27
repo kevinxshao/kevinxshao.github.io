64657ae57acb22bb2ea381b098ba5fba4e58acc2
kevinxshao <kevinxshao@gmail.com>
2021-05-27 15:14:24 -0400

    trying to fix slow chrome

--- indexload.js
+++ indexload.js
@@ -5,3 +5,7 @@ function showPage() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("wholeweb").style.display = "block";
 }
+if (navigator.appVersion.indexOf("Chrome/") != -1) {
+    var x = document.getElementsByClassName("glow");
+    x.style.animation="none";
+}
