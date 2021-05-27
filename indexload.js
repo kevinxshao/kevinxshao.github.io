b37ba23b367048b19642e08ee9b588eddb17597d
kevinxshao <kevinxshao@gmail.com>
2021-05-27 16:19:25 -0400

    trying to fix slow chrome

--- indexload.js
+++ indexload.js
@@ -9,9 +9,8 @@ if (navigator.appVersion.indexOf("Chrome/") != -1 && window.innerWidth < 700) {
     document.getElementById("logotext").classList.remove('glow');
     document.getElementById("logotext").classList.add('glownone');
     document.getElementById("logintext").classList.remove('glow2');
-    document.getElementById("logintext").classList.add('glownone');
+    document.getElementById("logintext").classList.add('glownone2');
     document.getElementById("signuptext").classList.remove('signupbtn');
     document.getElementById("signuptext").classList.add('signupbtnnone');
     window.alert("yes");
-    
 }
