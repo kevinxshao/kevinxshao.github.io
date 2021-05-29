2b58b5de144537d6d946266736edffeda4a9de41
kevinxshao <kevinxshao@gmail.com>
2021-05-29 19:28:51 -0400

    updated xsd and xml for frogotpassword page

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ function login(){
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
     firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      //window.alert("I'm in");
+      // ...
+    })
     .catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
