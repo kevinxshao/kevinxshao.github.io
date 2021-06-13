f34b36a4985c3c058f1feae70e45800512f73115
kevinxshao <kevinxshao@gmail.com>
2021-06-13 01:02:47 -0400

    fixed birthday date to see if it works

--- LoggedinMenu/Birthday/Aleena/aleena.js
+++ LoggedinMenu/Birthday/Aleena/aleena.js
@@ -1,6 +1,6 @@
 firebase.auth().onAuthStateChanged(function(user) {
     var d = new Date();
-    if (user && user.email=="stlkscar@hotmail.com" && d.getFullYear() == 2021 && d.getMonth() == 5 && d.getDate() == 5) {
+    if (user && user.email=="stlkscar@hotmail.com" && d.getFullYear() == 2021 && d.getMonth() == 5 && d.getDate() == 13) {
       // User is signed in.
     } else {
         window.location.href = '../../../index.xml';
--- indexbirthday.js
+++ indexbirthday.js
@@ -2,7 +2,7 @@ function choosebirthday(){
     var user = firebase.auth().currentUser;
     var userEmail = user.email;
     var d = new Date();
-    if(userEmail=="stlkscar@hotmail.com" && d.getFullYear() == 2021 && d.getMonth() == 5 && d.getDate() == 5){
+    if(userEmail=="stlkscar@hotmail.com" && d.getFullYear() == 2021 && d.getMonth() == 5 && d.getDate() == 13){
         window.location.href = 'LoggedinMenu/Birthday/Aleena/aleena.xml';
     }
     else{
