11b58cd475a38e2a24e2d9e2f95815e64d617b43
kevinxshao <kevinxshao@gmail.com>
2021-06-15 21:44:20 -0400

    update bdayd ate

--- LoggedinMenu/Birthday/Aleena/aleena.js
+++ LoggedinMenu/Birthday/Aleena/aleena.js
@@ -1,6 +1,6 @@
 firebase.auth().onAuthStateChanged(function(user) {
     var d = new Date();
-    if (user && user.email=="stlkscar@hotmail.com" && d.getFullYear() == 2021 && d.getMonth() == 5 && d.getDate() == 14) {
+    if (user && user.email=="stlkscar@hotmail.com" && d.getFullYear() == 2021 && d.getMonth() == 5 && d.getDate() == 15) {
       // User is signed in.
     } else {
         window.location.href = '../../../index.xml';
--- indexbirthday.js
+++ indexbirthday.js
@@ -2,7 +2,7 @@ function choosebirthday(){
     var user = firebase.auth().currentUser;
     var userEmail = user.email;
     var d = new Date();
-    if(userEmail=="stlkscar@hotmail.com" && d.getFullYear() == 2021 && d.getMonth() == 5 && d.getDate() == 14){
+    if(userEmail=="stlkscar@hotmail.com" && d.getFullYear() == 2021 && d.getMonth() == 5 && d.getDate() == 15){
         window.location.href = 'LoggedinMenu/Birthday/Aleena/aleena.xml';
     }
     else{
